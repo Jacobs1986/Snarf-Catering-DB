514f519df5244de1c21398ce8d9b035183e722f2
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-18 16:53:43 -0600

    Update all.js
    Removed id from the dive and gave it to the button

--- app/public/assets/js/all.js
+++ app/public/assets/js/all.js
@@ -3,8 +3,7 @@ $(function () {
     $.get("/api/all", (data) => {
         console.log(data);
         data.forEach(element => {
-            let button = $("<button>")
-            let card = $(`<div class='client-card container' id=${element.id}>`);
+            let card = $(`<div class='client-card container'>`);
             let row = $("<div class='row'>");
             let col1 = $("<div class='col-4 column-1'>");
             let col2 = $("<div class='col'>")
@@ -13,9 +12,7 @@ $(function () {
             col1.append(`<p>Address: ${element.address}</p>`);
             col1.append(`<p>Email: ${element.email}</p>`);
             col1.append(`<p>Phone: ${element.phone}</p>`);
-            button.text("Update");
-            button.addClass("edit")
-            col2.append(button);
+            col2.append(`<button type='button' class='edit-button' id='${element.id}'>Update</button>`);
             row.append(col1);
             row.append(col2);
             card.append(row);
