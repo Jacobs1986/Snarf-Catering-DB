5040126846f949c973beb69df0b57195e77d7350
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-19 13:27:09 -0600

    Add button Loaded
    The add button has been successfully added. Took a little time to figure out where to put the file so it loaded correctly, but it all works!

---
+++ public/assets/js/buttons-index.js
@@ -0,0 +1,19 @@
+console.log("FILE IS LINKED!");
+
+// add a new client
+$("#add-client-btn").on("click", event => {
+    console.log("This button is linked");
+    let newClient = {
+        contactname: $("#client-name").val().trim(""),
+        organization: $("#client-organization").val().trim(""),
+        address: $("#client-address").val().trim(""),
+        email: $("#client-email").val().trim(""),
+        phone: $("#client-phone").val().trim("")
+    }
+    console.log(newClient);
+    // Send to the database
+    $.post("/api/addclient", newClient).then(data => {
+        console.log(data);
+        location.reload();
+    })
+})
