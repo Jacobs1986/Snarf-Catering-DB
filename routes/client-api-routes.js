cf7aac48ce9158193b8494a8e12b78de16f80150
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-19 11:51:28 -0600

    Save route created
    I created the route for saving new client info to the API. I was getting an error for a while becasue the 'client' in the models has a lower case 'c'. When I ran it through the Postman organization was coming up as Null. It was because the column name was misspelled.

--- routes/client-api-routes.js
+++ routes/client-api-routes.js
@@ -1,11 +1,13 @@
 // Dependencies
-const db = require("../models");
+var db = require("../models");
 
 // Routes
-module.exports = (app) => {
+module.exports = function(app) {
     // Save a new client information to the database
     app.post("/api/addclient", (request, response) => {
-        db.Client.create(request.body).then(result => {
+        let newClient = request.body
+        console.log(newClient);
+        db.Client.create(newClient).then(result => {
             response.json(result);
         });
     });
