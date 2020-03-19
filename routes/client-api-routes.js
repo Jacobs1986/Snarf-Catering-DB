1e4a54eac19aa8a3b1e80f9637c998f37183c43e
Sean Jacobson <sean.jacobson86@gmail.com>
2020-03-19 11:56:56 -0600

    Update client-api-routes.js
    Added a get route to get all of the clients from the database.

--- routes/client-api-routes.js
+++ routes/client-api-routes.js
@@ -3,6 +3,13 @@ var db = require("../models");
 
 // Routes
 module.exports = function(app) {
+    // get all the information on clients
+    app.get("/api/clients", (request, response) => {
+        db.Client.findAll({}).then(results => {
+            response.json(results);
+        })
+    })
+
     // Save a new client information to the database
     app.post("/api/addclient", (request, response) => {
         let newClient = request.body
