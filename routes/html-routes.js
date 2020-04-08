d9ea58f2384863ca5ac08769528b237515c3c444
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-08 10:47:20 -0600

    Setting links
    Setting the links for the customer page.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,4 +7,8 @@ module.exports = (app) => {
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
+
+    app.get("/orders", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/order-history.html"));
+    })
 }
