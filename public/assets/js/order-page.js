5fc9859716da3d1fc9bbb620763c9b1afa189259
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-08 11:26:52 -0600

    Customer Info Shown
    All customer information is displaying correctly

--- public/assets/js/order-page.js
+++ public/assets/js/order-page.js
@@ -1,3 +1,14 @@
-// first get the customer id from the url
-var url = window.location.search
-var customerId = url.split("=")[1];
+$(document).ready(function () {
+    // first get the customer id from the url
+    var url = window.location.search;
+    var customerId = url.split("=")[1];
+
+    $.get(`/api/customers/${customerId}`, data => {
+        console.log(data);
+        $("#organization").text(data.organization);
+        $("#name").text(data.contactname);
+        $("#address").text(data.address);
+        $("#email").text(data.email);
+        $("#phone").text(data.phone);
+    })
+})
