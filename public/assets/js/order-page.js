bfdb77da3d411c8bc6af1dcbd9c7d36bd35fc46d
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-08 11:46:07 -0600

    Update order-page.js
    Gathered all the information into the variable order

--- public/assets/js/order-page.js
+++ public/assets/js/order-page.js
@@ -4,11 +4,26 @@ $(document).ready(function () {
     var customerId = url.split("=")[1];
 
     $.get(`/api/customers/${customerId}`, data => {
-        console.log(data);
         $("#organization").text(data.organization);
         $("#name").text(data.contactname);
         $("#address").text(data.address);
         $("#email").text(data.email);
         $("#phone").text(data.phone);
     })
+
+    $("#add-order").on("click", event => {
+        event.preventDefault();
+        console.log("The button is working")
+        let order = {
+            date: getInfo("#orderdate"),
+            orderType: getInfo("#ordertype"),
+            numofItems: getInfo("#numberofitems"),
+            total: getInfo("#total"),
+            notes: getInfo("#notes"),
+            CustomerId: customerId
+        }
+        console.log(order);
+    })
 })
+
+var getInfo = (input) => $(input).val().trim("")
