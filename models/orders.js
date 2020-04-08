acd041fd1fc90a4ac885ec8b8f4d5c4588c7b6b5
Sean Jacobson <sean.jacobson86@gmail.com>
2020-04-08 10:23:04 -0600

    Create orders.js

---
+++ models/orders.js
@@ -0,0 +1,35 @@
+module.exports = function(sequelize, DataTypes) {
+    var Order = sequelize.define("Order", {
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        orderType: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        numofItems: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        total: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        notes: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+    });
+
+    Order.associate = models => {
+        models.Order.belongsTo(models.Customer, {
+            onDelete: "CASCADE",
+            foreginKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Order
+}
