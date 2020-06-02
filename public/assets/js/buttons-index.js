0a9d0eb25f44a493918cfc150e105f77cf613fda
Sean Jacobson <sean.jacobson86@gmail.com>
2020-06-02 15:01:09 -0600

    Update buttons-index.js
    removed console.log

--- public/assets/js/buttons-index.js
+++ public/assets/js/buttons-index.js
@@ -4,7 +4,6 @@ var id;
 var deleteid;
 
 $("#add-customer-btn").on("click", event => {
-    console.log("This button is linked");
     let newcustomer = {
         contactname: $("#customer-name").val().trim(""),
         organization: $("#customer-organization").val().trim(""),
@@ -12,18 +11,14 @@ $("#add-customer-btn").on("click", event => {
         email: $("#customer-email").val().trim(""),
         phone: $("#customer-phone").val().trim("")
     }
-    console.log(newcustomer);
     // Send to the database
     $.post("/api/addcustomer", newcustomer).then(data => {
-        console.log(data);
         location.reload();
     })
 })
 
 // apply updates to a customer
 $("#update-customer-btn").on("click", event => {
-    console.log("This button is linked")
-    console.log(`The id that will be updated is ${id}`)
     let updatecustomer = {
         id: id,
         contactname: $("#update-name").val().trim(""),
@@ -32,14 +27,12 @@ $("#update-customer-btn").on("click", event => {
         email: $("#update-email").val().trim(""),
         phone: $("#update-phone").val().trim("")
     }
-    console.log(updatecustomer);
     // send to the database
     $.ajax({
         url: "/api/customers",
         method: "PUT",
         data: updatecustomer
     }).then(data => {
-        console.log(data);
         location.reload()
     })
 })
@@ -47,11 +40,9 @@ $("#update-customer-btn").on("click", event => {
 // show the information of a single customer
 $(document).on("click", ".edit-btn", function(event) {
     event.preventDefault();
-    console.log("This button is linked!");
     let classType = $(this).attr("class");
     id = $(this).attr("id");
     $.get(`/api/customers/${id}`, data => {
-       console.log(data);
        $("#update-name").val(data.contactname);
        $("#update-organization").val(data.organization);
        $("#update-address").val(data.address);
@@ -64,21 +55,16 @@ $(document).on("click", ".edit-btn", function(event) {
 // launch delete modal
 $(document).on("click", ".delete-btn", function(event) {
     event.preventDefault();
-    console.log("The button is live!");
     deleteid = $(this).attr("id");
-    console.log(`The id for this button is ${deleteid}`);
     $("#modal-customer-delete").modal("show");
 })
 
 // delete a customer button
 $("#delete-customer-btn").on("click", (event) => {
-    console.log("Delete button was clicked.");
-    console.log(deleteid);
     $.ajax({
         url: `/api/customers/${deleteid}`,
         method: "DELETE"
     }).then(data => {
-        console.log(data);
         location.reload();
     })
 })
