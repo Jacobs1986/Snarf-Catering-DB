b1a200c89523e514ebae11f747bb44ce94637b1b
Sean Jacobson <sean.jacobson86@gmail.com>
2020-07-02 18:01:58 -0600

    Setup react
    Set up the react app.

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      This is a test to make sure everything is working.
+    </div>
+  );
+}
+
+export default App;
