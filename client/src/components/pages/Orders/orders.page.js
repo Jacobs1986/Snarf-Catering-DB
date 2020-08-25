6d857d5e93581fd39d8e6d851f75c7b575b15b98
Sean Jacobson <sean.jacobson86@gmail.com>
2020-08-25 09:58:21 -0600

    Added ID and styling

--- client/src/components/pages/Orders/orders.page.js
+++ client/src/components/pages/Orders/orders.page.js
@@ -709,6 +709,7 @@ class Orders extends Component {
                     show={this.state.modalOrderEditShow}
                     close={this.handleModalClose}
                     name="modalOrderEditShow"
+                    orderNum="121860"
                 />
             </>
         );
