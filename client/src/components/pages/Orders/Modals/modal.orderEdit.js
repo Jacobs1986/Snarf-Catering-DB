d104155a2aa34ae52b9e990827a2be0fcbdbf86a
Sean Jacobson <sean.jacobson86@gmail.com>
2020-08-25 09:18:03 -0600

    Update modal.orderEdit.js
    Just setting up the modal

---
+++ client/src/components/pages/Orders/Modals/modal.orderEdit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Modal, Container, Row, Col, Button } from "react-bootstrap"
+
+function OrderEdit(props) {
+    return (
+        <div>This is a new component</div>
+    );
+}
+
+export default OrderEdit
