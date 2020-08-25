19c9ac50f1435066dac6ea115568cbca98b7e0c4
Sean Jacobson <sean.jacobson86@gmail.com>
2020-08-25 10:22:22 -0600

    Update modal.orderEdit.js

--- client/src/components/pages/Orders/Modals/modal.orderEdit.js
+++ client/src/components/pages/Orders/Modals/modal.orderEdit.js
@@ -8,7 +8,7 @@ function OrderEdit(props) {
             <Modal.Header closeButton>
                 <Form inline>
                     <Form.Label id="modal-edit-order-label">Order #:</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         id="modal-edit-orderNum-input"
                         type="text"
                         name="modalEditOrderNumber"
@@ -18,7 +18,30 @@ function OrderEdit(props) {
                 </Form>
             </Modal.Header>
             <Modal.Body>
-                <div>The contents will go here.</div>
+                <Form>
+                    <Form.Row className="row-spacing">
+                        {/* Order Date */}
+                        <Col md={3}>
+                            <Form.Label>Date:</Form.Label>
+                            <Form.Control
+                                type="date"
+                                name="newOrderDate"
+                                value={props.date}
+                                onChange={props.change}
+                            />
+                        </Col>
+                        {/* Order Number */}
+                        <Col md={3}>
+                            <Form.Label>Order/Receipt Number</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="modalEditOrderNumber"
+                                value={props.orderNumber.toUpperCase()}
+                                onChange={props.change}
+                            />
+                        </Col>
+                    </Form.Row>
+                </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="primary">Submit</Button>
