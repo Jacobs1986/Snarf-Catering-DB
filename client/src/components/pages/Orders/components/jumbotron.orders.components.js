cb0917c75e5e4e1f133d23c2dd9dd3ac8bb5b6b6
Sean Jacobson <sean.jacobson86@gmail.com>
2020-12-18 13:20:08 -0700

    Update jumbotron.orders.components.js

---
+++ client/src/components/pages/Orders/components/jumbotron.orders.components.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+// Bootstrap
+import { Container, Row, Col } from "react-bootstrap";
+
+class Jumbo extends Component {
+    state = {
+    }
+
+    render() {
+        return (
+            <div>This component is ready to be made</div>
+        )
+    }
+}
+
+export default Jumbo;
