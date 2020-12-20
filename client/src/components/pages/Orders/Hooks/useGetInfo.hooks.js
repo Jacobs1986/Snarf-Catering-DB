ea60c5e303240b8fbbc91938762d997c4c5041e7
Sean Jacobson <sean.jacobson86@gmail.com>
2020-12-20 15:57:15 -0700

    Update useGetInfo.hooks.js

---
+++ client/src/components/pages/Orders/Hooks/useGetInfo.hooks.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import API from "../../../../utils/API.orders";
+
+export const useGetInfo = () => {
+    const [orderInfo, setOrderInfo] = useState('');
+    
+};
