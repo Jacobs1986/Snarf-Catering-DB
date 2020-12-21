1cc24efd40ebbdd325952f762ce1db9eed4cf2b0
Sean Jacobson <sean.jacobson86@gmail.com>
2020-12-21 07:59:56 -0700

    Update orders.page.js

--- client/src/pages/Orders/orders.page.js
+++ client/src/pages/Orders/orders.page.js
@@ -1,6 +1,10 @@
 import React from "react";
 
+// functions
+import {customerInfo} from "./functions/customerInfo.functions";
+
 function Orders() {
+    console.log(customerInfo());
     return (
         <div>This page is ready!</div>
     );
