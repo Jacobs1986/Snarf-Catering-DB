d6e66660c1857f8be8bba39fbf75be92ec7f47f2
Sean Jacobson <sean.jacobson86@gmail.com>
2020-12-20 17:10:55 -0700

    Update customerInfo.function.js
    Wrote a test function

--- client/src/pages/Orders/functions/customerInfo.function.js
+++ client/src/pages/Orders/functions/customerInfo.function.js
@@ -1,1 +1,7 @@
-export const orderInfo
+export const orderInfo = (api) => {
+    let url = window.location.search;
+    let customerId = url.split("=")[1];
+    api.info(customerId).then(result => {
+        console.log(result);
+    })
+}
