dd0fbed9e330cb0fe31dd13fd975950abbdee01a
Sean Jacobson <sean.jacobson86@gmail.com>
2020-12-23 07:52:08 -0700

    Update orderDetails.modal.js

---
+++ client/src/pages/Orders/components/Modals/orderDetails.modal.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class OrderModal extends Component {
+    state = {
+    }
+
+    render() {
+        return (
+            <div>This component is ready to be made</div>
+        )
+    }
+}
+
+export default OrderModal;
