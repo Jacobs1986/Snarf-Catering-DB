5a08075645ebd7f62bffb998735d92043dddec98
Sean Jacobson <sean.jacobson86@gmail.com>
2020-12-23 13:40:56 -0700

    Update orderDetails.modal.js

--- client/src/pages/Orders/components/Modals/orderDetails.modal.js
+++ client/src/pages/Orders/components/Modals/orderDetails.modal.js
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
 
 // Bootstrap
-import { Modal } from "react-bootstrap"
+import { Modal, Container, Row, Col } from "react-bootstrap"
 
 class OrderModal extends Component {
-   constructor(props) {
-       super(props);
-       this.state = {
-           info: ""
-       }
-   }
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: ""
+        }
+    }
 
-   componentDidUpdate(prevProps) {
-       if (prevProps.show !== this.props.show) {
-           this.setState({
-               info: this.props.info
-           }, () => {
-               console.log(this.state.info)
-           })
-       }
-   }
+    componentDidUpdate(prevProps) {
+        if (prevProps.show !== this.props.show) {
+            this.setState({
+                info: this.props.info
+            }, () => {
+                console.log(this.state.info)
+            })
+        }
+    }
 
     render() {
         return (
@@ -28,7 +28,22 @@ class OrderModal extends Component {
                     <Modal.Title>Order #{this.state.info.orderNumber}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    This modal is ready for information.
+                    <Container>
+                        <Row>
+                            <Col md="auto">
+                                <h5>Date:</h5>
+                                <h6>{this.state.info.date}</h6>
+                            </Col>
+                            <Col md="auto">
+                                <h5>Number of Items:</h5>
+                                <h5>{this.state.info.numofItems}</h5>
+                            </Col>
+                            <Col md="auto">
+                                <h5>Total:</h5>
+                                <h5>${this.state.info.total}</h5>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Modal.Body>
             </Modal>
         )
