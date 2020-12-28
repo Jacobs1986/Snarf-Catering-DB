e6e681601563b77090e1248ca80fa2c6fc956e7a
Sean Jacobson <sean.jacobson86@gmail.com>
2020-12-28 13:17:47 -0700

    Update calc.largesandwich.js
    Updated function

--- client/src/components/pages/Orders/functions/calc.largesandwich.js
+++ client/src/components/pages/Orders/functions/calc.largesandwich.js
@@ -1,4 +1,4 @@
-export const largeSandwichCalc= (name, value, price1) => {
+export const largeSandwichCalc = (name, value, price1, price2, price3) => {
     var total;
     var stateName;
     switch (name) {
@@ -6,11 +6,19 @@ export const largeSandwichCalc= (name, value, price1) => {
             stateName = "totalRegNovice"
             total = parseFloat(value) * parseFloat(price1)
             return [stateName, total.toFixed(2)]
+        case "quantityRegSnarf":
+            stateName = "totalRegSnarf"
+            total = parseFloat(value) * parseFloat(price2)
+            return [stateName, total.toFixed(2)]
+        case "quantityRegPro":
+            stateName = "totalRegPro"
+            total = parseFloat(value) * parseFloat(price3)
+            return [stateName, total.toFixed(2)]
         default:
     }
 }
 
-export const largeSandwichTotal = (total1) => {
-    let total = parseFloat(total1);
+export const largeSandwichTotal = (total1, total2, total3) => {
+    let total = parseFloat(total1) + parseFloat(total2) + parseFloat(total3);
     return ["largeSandwichTotal", total.toFixed(2)];
 }
