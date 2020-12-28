a7dee9f417f2c9df74d7f2dfe17747e8ce79451d
Sean Jacobson <sean.jacobson86@gmail.com>
2020-12-28 15:15:33 -0700

    Update calc.largesandwich.js
    updated function

--- client/src/components/pages/Orders/functions/calc.largesandwich.js
+++ client/src/components/pages/Orders/functions/calc.largesandwich.js
@@ -1,4 +1,4 @@
-export const largeSandwichCalc = (name, value, price1, price2, price3) => {
+export const largeSandwichCalc = (name, value, price1, price2, price3, price4, price5) => {
     var total;
     var stateName;
     switch (name) {
@@ -14,12 +14,20 @@ export const largeSandwichCalc = (name, value, price1, price2, price3) => {
             stateName = "totalRegPro"
             total = parseFloat(value) * parseFloat(price3)
             return [stateName, total.toFixed(2)]
+        case "quantitySpecSnarf":
+            stateName = "totalSpecSnarf"
+            total = parseFloat(value) * parseFloat(price4)
+            return [stateName, total.toFixed(2)]
+        case "quantitySpecPro":
+            stateName = "totalSpecPro"
+            total = parseFloat(value) * parseFloat(price5)
+            return [stateName, total.toFixed(2)]
         default:
             console.log("There is something wrong!")
     }
 }
 
-export const largeSandwichTotal = (total1, total2, total3) => {
-    let total = parseFloat(total1) + parseFloat(total2) + parseFloat(total3);
+export const largeSandwichTotal = (total1, total2, total3, total4, total5) => {
+    let total = parseFloat(total1) + parseFloat(total2) + parseFloat(total3) + parseFloat(total4) + parseFloat(total5);
     return ["largeSandwichTotal", total.toFixed(2)];
 }
