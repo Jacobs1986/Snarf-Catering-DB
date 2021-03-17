ef78f39a121f088ee00d4cd023d830ca4619bdb5
Sean Jacobson <sean.jacobson86@gmail.com>
2021-03-17 07:16:42 -0600

    Adding some new code

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const customer = require("./customer.routes");
-const order = require("./orders.routes");
-
-// Routers
-router.use("/customer", customer);
-router.use("/order", order);
-
-module.exports = router;
